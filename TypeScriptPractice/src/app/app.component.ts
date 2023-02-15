a97553ca8eaba906577789bd3d6c9d7255208dc4
DTanasic <dusantanasic44@gmail.com>
2023-02-15 15:23:58 +0100

    Candidate class

--- TypeScriptPractice/src/app/app.component.ts
+++ TypeScriptPractice/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TypeScriptPractice';
+
+  ngOnInit(): void {
+  class Candidate {
+    firstName:string;
+    lastName:string;
+    birthday:Date;
+
+    constructor (firstName:string,lastName:string,birthday:Date){
+      this.firstName=firstName;
+      this.lastName=lastName;
+      this.birthday=birthday;
+    }
+
+  }
+    const candidate = new Candidate('Petar','Petrovic',new Date(2001 , 1,1))
+    const candidate1 = new Candidate('Marko','Markovic',new Date(1980 , 21,1))
+    console.log(candidate,candidate1);
+  }
+
 }
+
