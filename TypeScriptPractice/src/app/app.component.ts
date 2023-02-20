7203133b09568729ddddbef2e342a1bb0d35365a
DTanasic <dusantanasic44@gmail.com>
2023-02-20 19:21:18 +0100

    Employee class)

--- TypeScriptPractice/src/app/app.component.ts
+++ TypeScriptPractice/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Employee } from './models/employee.model';
 import { Component, OnInit } from '@angular/core';
 import { Candidate } from './models/candidate.model';
 
@@ -10,17 +11,20 @@ export class AppComponent implements OnInit {
   title = 'TypeScriptPractice';
 
   ngOnInit(): void {
-
     const candidate = new Candidate('Petar','Petrovic',new Date(2001 , 1,1))
     const candidate1 = new Candidate('Marko','Markovic',new Date(1980 , 21,1))
     console.log(candidate,candidate1);
-
 
     let candidateArray:Candidate[]= [candidate,candidate1];
     console.log(candidateArray);
     const candidates:Candidate[]=[candidate,candidate1]
     const displayMessage=this.printCandidates(candidates)
     console.log(displayMessage);
+
+    const employee=new Employee(1 ,"Marko","marko@gmail.com",38169111222,"FrontEnd");
+    const employee1=new Employee(2,"Petar","petar@gmail.com",381654444,"BackEnd");
+    console.log(employee,employee1);
+
   }
   printCandidates (candidates:Candidate[]) {
     let message:string="";
