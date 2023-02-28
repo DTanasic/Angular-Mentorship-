bfc3c296418c3175936a10d6e29575cfcf055d42
DTanasic <dusantanasic44@gmail.com>
2023-02-28 18:57:39 +0100

    search value

--- GoldenBook/src/app/shared/bars/bars.component.ts
+++ GoldenBook/src/app/shared/bars/bars.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-bars',
   templateUrl: './bars.component.html',
-  styleUrls: ['./bars.component.scss']
+  styleUrls: ['./bars.component.scss'],
 })
 export class BarsComponent implements OnInit {
+  constructor() {}
+  displaySearch: string = '';
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getSearchValue(src: string) {
+    this.displaySearch = src;
   }
-
 }
