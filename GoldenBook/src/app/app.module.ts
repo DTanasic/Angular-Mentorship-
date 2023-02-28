faa8f42d8c4b651a9c7645c95f02b529c6df4589
DTanasic <dusantanasic44@gmail.com>
2023-02-28 18:00:38 +0100

    structure fix v3

--- GoldenBook/src/app/app.module.ts
+++ GoldenBook/src/app/app.module.ts
@@ -1,24 +1,12 @@
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { BarsComponent } from './bars/bars.component';
-import { BooksComponent } from './books/books.component';
-import { RowsComponent } from './rows/rows.component';
-import { BookComponent } from './book/book.component';
-import { HeaderComponent } from './core/components/header/header.component';
-import { FooterComponent } from './core/components/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent,
-    HeaderComponent,
-    BarsComponent,
-    BooksComponent,
-    RowsComponent,
-    BookComponent,
-  ],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CoreModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- GoldenBook/src/app/core/core.module.ts
+++ GoldenBook/src/app/core/core.module.ts
@@ -1,12 +1,12 @@
+import { AppComponent } from './../app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
-
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [FooterComponent, HeaderComponent],
+  imports: [CommonModule],
+  exports: [FooterComponent, HeaderComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
