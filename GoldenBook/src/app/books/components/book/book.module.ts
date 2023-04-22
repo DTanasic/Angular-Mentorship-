d84b720caf96beb206aea7dc7e692dc2f2459d6c
DTanasic <dusantanasic44@gmail.com>
2023-04-22 13:47:15 +0100

    issue #81 fix

--- GoldenBook/src/app/books/components/book/book.module.ts
+++
@@ -1,9 +0,0 @@
-import { SharedModule } from './../../../shared/shared.module';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-@NgModule({
-  declarations: [],
-  imports: [CommonModule, SharedModule],
-})
-export class BookModule {}
